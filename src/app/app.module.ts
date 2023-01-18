b1967405890bf4f34731a41792804067f1ac8b98
saracsilva <saracarolinasilva@gmail.com>
2023-01-18 20:34:32 +0100

    added routes and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NerdflixComponent } from './components/nerdflix/nerdflix.component';
+import { MoviesComponent } from './components/movies/movies.component';
+
+const appRoutes: Routes = [
+  { path: '', component: NerdflixComponent },
+  { path: 'movies', component: MoviesComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, NerdflixComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    NerdflixComponent,
+    MoviesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
