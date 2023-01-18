a8fb41362003e37e9a7875adc940db22f8d4bfe0
saracsilva <saracarolinasilva@gmail.com>
2023-01-18 22:53:12 +0100

    home page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CommonModule } from '@angular/common';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NerdflixComponent } from './components/nerdflix/nerdflix.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { SeriesComponent } from './components/series/series.component';
+
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
+export function playerFactory() {
+  return player;
+}
 
 const appRoutes: Routes = [
   { path: '', component: NerdflixComponent },
   { path: 'movies', component: MoviesComponent },
+  { path: 'series', component: SeriesComponent },
 ];
 
 @NgModule({
@@ -18,10 +29,13 @@ const appRoutes: Routes = [
     NavbarComponent,
     NerdflixComponent,
     MoviesComponent,
+    SeriesComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { enableTracing: true }),
+    CommonModule,
+    [LottieModule.forRoot({ player: playerFactory })],
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/nerdflix/nerdflix.component.ts
+++ src/app/components/nerdflix/nerdflix.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-nerdflix',
   templateUrl: './nerdflix.component.html',
-  styleUrls: ['./nerdflix.component.css']
+  styleUrls: ['./nerdflix.component.css'],
 })
 export class NerdflixComponent {
+  options: AnimationOptions = {
+    path: '/assets/nerds_logo_01.json',
+    autoplay: true,
+    loop: false,
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // This is the component function that binds to the animationCreated event from the package
+  onAnimate(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
 }
