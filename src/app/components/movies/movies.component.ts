8aceab87546d8adef07a5242dc003152ae4aa5b7
saracsilva <saracarolinasilva@gmail.com>
2023-01-19 10:53:05 +0100

    getting the movie list

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import movies from './imdb-top-50.json';
 
+interface Movie {}
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.css']
+  styleUrls: ['./movies.component.css'],
 })
 export class MoviesComponent {
-
+  moviesList: [{}] = movies.data.movies;
+  console = console;
 }
